876e43a8bc72299360b54d909b47bc4599b1390f
samuelmachado <samuel.silva39@fatec.sp.gov.br>
2020-07-27 08:18:28 -0300

    módulo 4

--- app.py
+++ app.py
@@ -1,22 +1,30 @@
-from flask import Flask, request, Response, redirect, url_for, render_template
-from flask_bootstrap import Bootstrap
+from datetime import timedelta
+from flask import Flask, render_template, redirect, request, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_required, login_user, logout_user
+from werkzeug.security import generate_password_hash, check_password_hash
 
-#CONFIG
 app = Flask(__name__)
-db = SQLAlchemy(app)
-app.config["SQLALCHEMY_DATABASE_URI"]="sqlite:///app.db"
+app.config["SECRET_KEY"] = 'secret'
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-Bootstrap(app)
 
-#MODELS
-class User(db.Model):
+db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+
+
+@login_manager.user_loader
+def current_user(user_id):
+    return User.query.get(user_id)
+
+
+class User(db.Model, UserMixin):
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(84), nullable=False)
     email = db.Column(db.String(84), nullable=False, unique=True, index=True)
     password = db.Column(db.String(255), nullable=False)
-    profile = db.relationship('Profile', backref='user')
+    profile = db.relationship('Profile', backref='user', uselist=False)
 
     def __str__(self):
         return self.name
@@ -24,54 +32,76 @@ class User(db.Model):
 class Profile(db.Model):
     __tablename__ = "profiles"
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(84), nullable=False)
     photo = db.Column(db.Unicode(124), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+
+
     def __str__(self):
         return self.name
 
-#ROTAS
 @app.route("/")
 def index():
-    users = User.query.all()
-    return render_template("user.html", users=users)
+    users = User.query.all() # Select * from users; 
+    return render_template("users.html", users=users)
 
-@app.route("/redirect-me")
-def redirectMe():
-    return redirect(url_for("response"))
-
-@app.route("/response")
-def response():
-    headers = {
-        "Content-Type": "text/html"
-    }
-    return render_template("response.html")
-
-@app.route("/view/<int:id>")
-def view(id):
+@app.route("/user/<int:id>")
+@login_required
+def unique(id):
     user = User.query.get(id)
-    return render_template("view_user.html", user=user)
+    return render_template("user.html", user=user)
 
 @app.route("/user/delete/<int:id>")
 def delete(id):
     user = User.query.filter_by(id=id).first()
     db.session.delete(user)
-    # db.session.commit()
+    db.session.commit()
+
     return redirect("/")
-@app.route("/posts/<int:id>")
-def posts(id):
-    titulo = request.args.get("titulo")
-    data = dict(
-        path=request.path,
-        referrer=request.referrer,
-        content_type=request.content_type,
-        method=request.method,
-        id=id if id else 0
-    )
-    return data
-    return "Página inicial"
 
 
-#INIT
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        user = User()
+        user.name = request.form["name"]
+        user.email = request.form["email"]
+        user.password = generate_password_hash(request.form["password"])
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for("index"))
+
+    return render_template("register.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        email = request.form["email"]
+        password = request.form["password"]
+        remember = request.form["remember"]
+
+        user = User.query.filter_by(email=email).first()
+
+        if not user:
+            flash("Credênciais incorretas")
+            return redirect(url_for("login"))
+
+        if not check_password_hash(user.password, password):
+            flash("Credênciais incorretas")
+            return redirect(url_for("login"))
+
+        login_user(user, remember=remember, duration=timedelta(days=7))
+        return redirect(url_for("index"))
+
+    return render_template("login.html")
+
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("index"))
+
 if __name__ == "__main__":
     app.run(debug=True)
